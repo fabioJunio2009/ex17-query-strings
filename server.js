fb4a93b08f048622a91ad6fb6cb3bf328506d32f
Fábio Júnio Oliveira da Silva Filho <fjosf1@aluno.ifal.edu.br>
2026-06-15 11:27:08 -0300

    add query router server.js

--- server.js
+++ server.js
@@ -5,5 +5,14 @@ const app = express();
 // Ex: /busca?q=node          → { "q": "node" }
 //     /busca?q=js&pagina=2   → { "q": "js", "pagina": "2" }
 //     /busca (sem parâmetros) → { "q": "" }
+app.use(express.json());
+
+app.get("/busca", (req, res)=>{
+    res.type("aplication/json")
+    const query = req.query
+    query.q = query.q ?? ""
+    res.status(200).json(query)
+})
+
 
 app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
